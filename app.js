0ebf288f5b6d34c4b0de2b79bbf092570346b1ab
Aman Brhane <117799650+Ambesawi@users.noreply.github.com>
2024-01-31 19:13:49 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,35 +1,45 @@
+// Load environment variables from the .env file
 require("dotenv").config();
-const createError = require("http-errors");
-const express = require("express");
-const path = require("path");
-const cookieParser = require("cookie-parser");
-const logger = require("morgan");
-const mongoose = require("mongoose");
-const session = require("express-session");
-const passport = require("passport");
-const flash = require("connect-flash");
-const Category = require("./models/category");
-var MongoStore = require("connect-mongo")(session);
-const connectDB = require("./config/db");
 
+// Import necessary libraries and modules
+const createError = require("http-errors");  // Module for creating HTTP errors
+const express = require("express");  // Express.js is a web application framework for Node.js
+const path = require("path");  // Node.js module for working with file paths
+const cookieParser = require("cookie-parser");  // Middleware for parsing cookies
+const logger = require("morgan");  // HTTP request logger middleware
+const mongoose = require("mongoose");  // Mongoose is an ODM library for MongoDB
+const session = require("express-session");  // Session middleware for Express
+const passport = require("passport");  // Authentication middleware
+const flash = require("connect-flash");  // Middleware for handling flash messages
+const Category = require("./models/category");  // Import Category model
+const MongoStore = require("connect-mongo")(session);  // MongoDB session store
+const connectDB = require("./config/db");  // Database connection configuration
+
+// Create an instance of the Express application
 const app = express();
+
+// Configure passport for authentication
 require("./config/passport");
 
-// mongodb configuration
+// Connect to the MongoDB database
 connectDB();
-// view engine setup
+
+// Set up view engine
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-// admin route
+// Define admin routes
 const adminRouter = require("./routes/admin");
 app.use("/admin", adminRouter);
 
+// Configure middleware and static file serving
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+
+// Configure session handling
 app.use(
   session({
     secret: process.env.SESSION_SECRET,
@@ -38,15 +48,16 @@ app.use(
     store: new MongoStore({
       mongooseConnection: mongoose.connection,
     }),
-    //session expires after 3 hours
-    cookie: { maxAge: 60 * 1000 * 60 * 3 },
+    cookie: { maxAge: 60 * 1000 * 60 * 3 }, // Session expires after 3 hours
   })
 );
+
+// Configure flash messages and passport authentication
 app.use(flash());
 app.use(passport.initialize());
 app.use(passport.session());
 
-// global variables across routes
+// Set global variables across routes
 app.use(async (req, res, next) => {
   try {
     res.locals.login = req.isAuthenticated();
@@ -61,14 +72,14 @@ app.use(async (req, res, next) => {
   }
 });
 
-// add breadcrumbs
-get_breadcrumbs = function (url) {
+// Add breadcrumbs to the request object
+const getBreadcrumbs = function (url) {
   var rtn = [{ name: "Home", url: "/" }],
-    acc = "", // accumulative url
+    acc = "",
     arr = url.substring(1).split("/");
 
   for (i = 0; i < arr.length; i++) {
-    acc = i != arr.length - 1 ? acc + "/" + arr[i] : null;
+    acc = i !== arr.length - 1 ? acc + "/" + arr[i] : null;
     rtn[i + 1] = {
       name: arr[i].charAt(0).toUpperCase() + arr[i].slice(1),
       url: acc,
@@ -76,12 +87,14 @@ get_breadcrumbs = function (url) {
   }
   return rtn;
 };
+
+// Middleware to add breadcrumbs to the request object
 app.use(function (req, res, next) {
-  req.breadcrumbs = get_breadcrumbs(req.originalUrl);
+  req.breadcrumbs = getBreadcrumbs(req.originalUrl);
   next();
 });
 
-//routes config
+// Configure routes for different modules
 const indexRouter = require("./routes/index");
 const productsRouter = require("./routes/products");
 const usersRouter = require("./routes/user");
@@ -91,26 +104,29 @@ app.use("/user", usersRouter);
 app.use("/pages", pagesRouter);
 app.use("/", indexRouter);
 
-// catch 404 and forward to error handler
+// Catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
 });
 
-// error handler
+// Error handler
 app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
+  // Render the error page
   res.status(err.status || 500);
   res.render("error");
 });
 
+// Set the port for the server
 var port = process.env.PORT || 3000;
 app.set("port", port);
+
+// Start the server and listen on the specified port
 app.listen(port, () => {
   console.log("Server running at port " + port);
 });
 
+// Export the app for external use
 module.exports = app;
